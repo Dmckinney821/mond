6baa1f759d89790d9804c2c3475970c4b4ef90ae
Dan McKinney <3dhdaniel@gmail.com>
2020-04-06 18:54:43 -0400

    spicy sausage pasta

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,11 +1,23 @@
 class DocsController < ApplicationController
+
+    before_action :find_doc, only: [:show, :edit, :update, :destroy]
+
     def index
+        @docs = Doc.all.order("created_at DESC")
     end
     
     def show
+        
     end
 
     def create
+        @doc = Doc.new(doc_params)
+        
+        if @doc.save
+            redirect_to @doc
+        else 
+            render 'new'
+        end
     end
 
     def edit
@@ -15,6 +27,7 @@ class DocsController < ApplicationController
     end
 
     def new
+        @doc = Doc.new
     end
 
     def destroy
@@ -23,8 +36,10 @@ class DocsController < ApplicationController
     private
         
         def find_doc
+            @doc = Doc.find(params[:id])
         end
 
         def doc_params
+            params.require(:doc).permit(:title, :content)
         end
 end
