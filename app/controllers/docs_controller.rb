6b915d541727dfe18b4faca92db4fdae64ba146f
Dan McKinney <3dhdaniel@gmail.com>
2020-04-06 19:12:46 -0400

    whats for dessert

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,5 +1,4 @@
 class DocsController < ApplicationController
-
     before_action :find_doc, only: [:show, :edit, :update, :destroy]
 
     def index
@@ -7,15 +6,14 @@ class DocsController < ApplicationController
     end
     
     def show
-        
     end
 
     def new
-        @doc = Doc.new
+        @doc = current_user.docs.build
     end
 
     def create
-        @doc = Doc.new(doc_params)
+        @doc = current_user.docs.build(doc_params)
         
         if @doc.save
             redirect_to @doc
